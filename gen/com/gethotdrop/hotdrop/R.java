453081534f5c2d5642e05602e2c303426e203d45
Richard Higgins <relh@terpmail.umd.edu>
2013-10-22 17:14:03 -0400

    got rid of bloat

--- gen/com/gethotdrop/hotdrop/R.java
+++ gen/com/gethotdrop/hotdrop/R.java
@@ -31,21 +31,26 @@ public final class R {
         public static final int marginSmall=0x7f050003;
     }
     public static final class drawable {
-        public static final int desktop=0x7f020000;
+        public static final int camera=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int placeholderimage=0x7f020002;
+        public static final int post=0x7f020003;
+        public static final int upload=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int image=0x7f090004;
-        public static final int list=0x7f090003;
-        public static final int note=0x7f090005;
+        public static final int action_settings=0x7f09000c;
+        public static final int cameraButton=0x7f090003;
+        public static final int image=0x7f090007;
+        public static final int list=0x7f090006;
+        public static final int note=0x7f090008;
         public static final int post=0x7f090000;
+        public static final int postButton=0x7f090005;
         public static final int postImage=0x7f090001;
         public static final int postNote=0x7f090002;
-        public static final int score=0x7f090007;
-        public static final int separator=0x7f090006;
-        public static final int timestamp=0x7f090008;
+        public static final int score=0x7f09000a;
+        public static final int separator=0x7f090009;
+        public static final int timestamp=0x7f09000b;
+        public static final int uploadButton=0x7f090004;
     }
     public static final class layout {
         public static final int activity_feed=0x7f030000;
